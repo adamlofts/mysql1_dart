4a0a9b294c7e860be685c016e1d1238a3c496816
James Ots <code@jamesots.com>
2012-04-03 20:08:51 +0100

    Added ability to read mysql packets

--- lib/mysql.dart
+++ lib/mysql.dart
@@ -1,17 +1,78 @@
 class MySqlConnection implements Connection {
+  static final int HEADER_SIZE = 4;
+  static final int STATE_PACKET_HEADER = 0;
+  static final int STATE_PACKET_DATA = 1;
+  
   Map<String, Database> _dbs;
   String _host;
   String _user;
   String _password;
   int _port;
   Socket _socket;
+
+  List _headerBuffer;
+  List _dataBuffer;
+  
+  int _dataSize;
+  int _readPos = 0;
+  
+  int _state = STATE_PACKET_HEADER;
   
   MySqlConnection([String host='localhost', String user, String password, int port=3306]) {
     _host = host;
     _user = user;
     _password = password;
     _port = port;
-    _socket = new Socket(host, port);
+    
+    _headerBuffer = new List(HEADER_SIZE);
+    
+    _dbs = new Map<String, Database>();
+
+    _openConnection();
+  }
+  
+  void _openConnection() {
+    print("opening connection to $_host:$_port");
+    _socket = new Socket(_host, _port);
+    _socket.onClosed = () {
+      print("closed");
+    };
+    _socket.onConnect = () {
+      print("connected");
+    };
+    _socket.onData = _onData;
+    _socket.onError = (Exception e) {
+      print("exception $e");
+    };
+    _socket.onWrite = () {
+      print("write");
+    };
+  }
+  
+  void _onData() {
+    print("got data");
+    switch (_state) {
+    case STATE_PACKET_HEADER:
+      int bytes = _socket.readList(_headerBuffer, _readPos, HEADER_SIZE - _readPos);
+      _readPos += bytes;
+      if (_readPos == HEADER_SIZE) {
+        _state = STATE_PACKET_DATA;
+        _dataSize = _headerBuffer[0] + (_headerBuffer[1] << 8) + (_headerBuffer[2] << 16);
+        _readPos = 0;
+        print("about to read $_dataSize bytes for packet ${_headerBuffer[3]}");
+        _dataBuffer = new List(_dataSize);
+      }
+      break;
+    case STATE_PACKET_DATA:
+      int bytes = _socket.readList(_dataBuffer, _readPos, _dataSize - _readPos);
+      print("got $bytes bytes");
+      _readPos += bytes;
+      if (_readPos == _dataSize) {
+        print("read all data");
+        _state = STATE_PACKET_HEADER;
+      }
+      break;
+    }
   }
   
   Database openDatabase(String dbName) {
--- main.dart
+++ main.dart
@@ -1,19 +1,20 @@
 #import('lib/sqljockey.dart');
 
 void main() {
-  Connection cnx = new MySqlConnection(port:3307, user:'large', password:'large');
-  Database db = cnx.openDatabase('large');
-  
-  Results results = db.query('select * from projects');
-  print(results.count);
-  for (Result r in results) {
-    print(r.index);
-    print(r.value);
-  }
-  
-  Query q = db.prepare('select * from projects where fingerprint = ?');
-  q[0] = 135246234234;
-  results = q.execute();
-  
-  cnx.close();
+  Connection cnx = new MySqlConnection(user:'large', password:'large');
+  print("got connection");
+//  Database db = cnx.openDatabase('large');
+//  
+//  Results results = db.query('select * from projects');
+//  print(results.count);
+//  for (Result r in results) {
+//    print(r.index);
+//    print(r.value);
+//  }
+//  
+//  Query q = db.prepare('select * from projects where fingerprint = ?');
+//  q[0] = 135246234234;
+//  results = q.execute();
+//  
+//  cnx.close();
 }
