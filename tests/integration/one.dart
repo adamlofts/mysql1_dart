e6f851d0df1e83bd8455832c6e92bed3c1f06fe1
James Ots <code@jamesots.com>
2012-04-17 22:58:07 +0100

    Better integration test

--- tests/integration/one.dart
+++ tests/integration/one.dart
@@ -1,5 +1,9 @@
 class One {
+  List<String> tables;
+  
   void runAll() {
+    tables = ["integ", "integ2", "integ3"];
+    
     AsyncConnection cnx = new AsyncMySqlConnection();
     cnx.connect(user:'test', password:'test', db:'bob').then((nothing) {
       cnx.useDatabase('bob').then((dummy) {
@@ -9,18 +13,28 @@ class One {
   }
   
   void dropTables(AsyncConnection cnx) {
-    print("drop tables");
-    Future future = cnx.query("drop table integ");
+    String table = tables.last();
+    tables.removeLast();
+    print("drop table $table");
+    Future future = cnx.query("drop table $table");
     future.handleException((exception) {
       if (exception is MySqlError && exception.errorNumber == 1051) {
         print("no table to delete");
-        createTables(cnx);
+        if (tables.length == 0) {
+          createTables(cnx);
+        } else {
+          dropTables(cnx);
+        }
       }
       return true;
     });
     future.then((x) {
       print("deleted");
-      createTables(cnx);
+      if (tables.length == 0) {
+        createTables(cnx);
+      } else {
+        dropTables(cnx);
+      }
     });
   }
   
