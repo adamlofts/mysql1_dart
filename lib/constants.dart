f87c39f73a30daa5f50cbdd2c8b7a858f4b6129f
Kevin Moore <kevmoo@google.com>
2015-09-30 12:03:51 -0700

    add library name to constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,3 @@
-library constants;
-
 /* Copyright (C) 2000 MySQL AB
    Copyright (C) 2012 James Ots
 
@@ -15,6 +13,8 @@ library constants;
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+library sqljocky.constants;
 
 /*
  * This file is based on excerpts from include/mysql_com.h
