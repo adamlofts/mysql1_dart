f7c1fa59b27f9aa8c4ccfed99a696dc809ba2206
Kevin Moore <kevmoo@google.com>
2015-09-30 21:27:58 -0700

    cleanup Blob

--- lib/src/blob.dart
+++ lib/src/blob.dart
@@ -2,6 +2,10 @@ library sqljocky.blob;
 
 import 'dart:convert';
 
+import 'package:collection/collection.dart';
+
+const _listQuality = const ListEquality();
+
 /**
  * Holds blob data, and can be created or accessed as either a [String] or a [List] of
  * 8-bit integers.
@@ -10,46 +14,21 @@ import 'dart:convert';
  * integers are treated as UTF-8 code units (unsigned 8-bit integers).
  */
 class Blob {
-  String _string;
-  List<int> _codeUnits;
-  int _hashcode;
+  final List<int> _codeUnits;
 
   /// Create a [Blob] from a [string].
-  Blob.fromString(String string) {
-    this._string = string;
-  }
+  factory Blob.fromString(String string) => new Blob.fromBytes(UTF8.encode(string));
 
   /// Create a [Blob] from a list of [codeUnits].
-  Blob.fromBytes(List<int> codeUnits) {
-    this._codeUnits = codeUnits;
-  }
+  Blob.fromBytes(List<int> codeUnits) : this._codeUnits = codeUnits;
 
   /// Returns the value of the blob as a [String].
-  String toString() {
-    if (_string != null) {
-      return _string;
-    }
-    return UTF8.decode(_codeUnits, allowMalformed: true);
-  }
+  String toString() => UTF8.decode(_codeUnits, allowMalformed: true);
 
   /// Returns the value of the blob as a list of code units.
-  List<int> toBytes() {
-    if (_codeUnits != null) {
-      return _codeUnits;
-    }
-    return UTF8.encode(_string);
-  }
+  List<int> toBytes() => _codeUnits;
 
-  int get hashCode {
-    if (_hashcode == null) {
-      if (_string != null) {
-        _hashcode = _string.hashCode;
-      } else {
-        _hashcode = UTF8.decode(_codeUnits, allowMalformed: true).hashCode;
-      }
-    }
-    return _hashcode;
-  }
+  int get hashCode => _listQuality.hash(_codeUnits);
 
-  bool operator ==(other) => toString() == other.toString();
+  bool operator ==(Object other) => other is Blob && _listQuality.equals(_codeUnits, other.toBytes());
 }
