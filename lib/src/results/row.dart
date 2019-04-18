1723b6d023cca5a0bc5525b3dcecd97ae2a81f08
Bob Jackman <bob@liveninja.com>
2019-04-18 11:25:02 -0700

    Move some common functionality in both `StandardDataPacket` and `BinaryDataPacket` up to their parent class `Row`

--- lib/src/results/row.dart
+++ lib/src/results/row.dart
@@ -2,10 +2,41 @@ library mysql1.row;
 
 import 'dart:collection';
 
+import 'field.dart';
+import '../buffer.dart';
+
 /**
  * A row of data. Fields can be retrieved by index, or by name.
  *
  * When retrieving a field by name, only fields which are valid Dart
  * identifiers, and which aren't part of the List object, can be used.
  */
-abstract class Row extends ListBase<dynamic> {}
+abstract class Row extends ListBase<dynamic> {
+  /// Values as List
+  List<dynamic> values;
+
+  /// Values as Map
+  final Map<String, dynamic> fields = <String, dynamic>{};
+
+  int get length => values.length;
+
+  set length(int newLength) {
+    throw new UnsupportedError("Cannot set length of results");
+  }
+
+  dynamic operator [](dynamic index) {
+    if (index is int) {
+      return values[index];
+    } else {
+      return fields[index.toString()];
+    }
+  }
+
+  void operator []=(int index, dynamic value) {
+    throw new UnsupportedError("Cannot modify row");
+  }
+
+  String toString() => "Value: $values";
+
+  Object readField(Field field, Buffer buffer);
+}
