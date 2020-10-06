876a90134c9478bca64a8e8a6ac237e854ffb3ec
Adam Lofts <adam.lofts@gmail.com>
2020-10-06 14:11:44 +0100

    Allow malformed utf8 in messages
    Its not clear what the encoding of these messages is supposed to be.

--- lib/src/buffer.dart
+++ lib/src/buffer.dart
@@ -136,7 +136,7 @@ class Buffer {
 
   /// Reads a string of the given [length] from the buffer.
   String readString(int length) {
-    var s = utf8.decode(_list.sublist(_readPos, _readPos + length));
+    var s = utf8.decode(_list.sublist(_readPos, _readPos + length), allowMalformed: true);
     _readPos += length;
     return s;
   }
