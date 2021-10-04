2bda707b9fa051380aaf32619706085bb716ff1d
Graciliano Monteiro Passos <gracilianomp@gmail.com>
2021-10-04 20:10:13 -0300

    Export `TransactionContext`
    `TransactionContext` needs to be exported since the `transaction` method passes a `TransactionContext` to the `queryBlock` Function.

--- lib/mysql1.dart
+++ lib/mysql1.dart
@@ -5,7 +5,7 @@ export 'src/mysql_client_error.dart' show MySqlClientError;
 export 'src/mysql_exception.dart' hide createMySqlException;
 export 'src/mysql_protocol_error.dart' hide createMySqlProtocolError;
 export 'src/single_connection.dart'
-    show MySqlConnection, Results, ConnectionSettings;
+    show MySqlConnection, TransactionContext, Results, ConnectionSettings;
 
 export 'src/auth/character_set.dart';
 
